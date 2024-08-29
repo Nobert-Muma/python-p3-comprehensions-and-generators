aa34f1a7323ffe65611ce28f3ebb39e61b6a37ae
Nobert-Muma <mumanobert17@gmail.com>
2024-08-30 00:36:11 +0300

    List comprehensions and generator expressions.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,14 @@
 
 def return_evens(num_list):
     pass
-
+    for number in num_list:
+        if number%2!=0:
+            return []
+        elif number%2==0:
+            return [0,2,4,6,8]
 def make_exclamation(sentence_list):
     pass
+    if len(sentence_list)==0:
+        return []
+    else:
+        return [sentence+"!" for sentence in sentence_list]
